7688d8e25f2a89fcd971d86f5d3c2fd3d8b9616f
Sandesh Prabhu <77835063+sandesh-prabhu@users.noreply.github.com>
2021-05-11 10:14:55 +0530

    Create arduino_servo_and_gas_sensor.ino

---
+++ arduino_servo_and_gas_sensor.ino
@@ -0,0 +1,23 @@
+#include<Servo.h>
+Servo window;
+int GasPin = 7;
+int Gas = LOW;
+void setup() {
+  window.attach(9);
+  pinMode(GasPin,INPUT);
+  window.write(0);
+}
+void loop() {
+  Gas = digitalRead(GasPin);
+  if(Gas==HIGH)
+  {
+    Serial.println("FLAME");
+    window.write(0);
+    delay(3000);
+  }
+  else
+  {
+    window.write(90);
+    Serial.println("Normal");
+  }
+}
